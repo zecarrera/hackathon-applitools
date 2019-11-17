175fadf5eed8e1f8010992bf8707ebd826ad267b
Jose Neto <jose.neto@bglgroup.co.uk>
2019-11-17 00:36:01 +0000

    :white_check_mark: Adds extra visual checks for applitools

--- cypress/integration/visual_tests.spec.js
+++ cypress/integration/visual_tests.spec.js
@@ -4,10 +4,10 @@ import * as dashboardPage from "../../page-objects/dashboard-page";
 
 describe("When login page is loaded", () => {
     before(() => {
-        cy.eyesOpen({appName: 'hackaton-applitools', batchName: 'hackaton-applitools-login'});
+        cy.eyesOpen({appName: 'hackaton-applitools', batchName: 'login-form'});
         
         loginPage.navigate();
-        cy.eyesCheckWindow('login page loaded')
+        cy.eyesCheckWindow('login page loaded');
     });
 
     it("Then app logo is visible", () => {
@@ -44,45 +44,57 @@ describe("When login page is loaded", () => {
 const userAndPasswordInvalidInputs = [
     {
         username:"",
-        password:""
+        password:"",
+        testDescription: "noUsernameAndPassword"
     },
     {
         username:"testuser",
-        password:""
+        password:"",
+        testDescription: "noPassword"
     },
     {
         username:"",
-        password:"testpass"
+        password:"testpass",
+        testDescription: "noUsername"
     }
 ];
 userAndPasswordInvalidInputs
     .forEach(testInput => {
         describe("When user logs in with invalid data", () => {
             before(()=> {
+                cy.eyesOpen({appName: 'hackaton-applitools', batchName: `login-invalid-${testInput.testDescription}`});
+
                 loginPage.navigate();
                 loginPage.loginAs(testInput.username, testInput.password);
             });
 
             it("Then an error should be displayed", () =>{
                 loginPage.isWarningMessageDisplayed();
+                cy.eyesCheckWindow(`invalid login - error message shown - ${testInput.testDescription}`);
             });
+
+            after(() => cy.eyesClose());
         });
     });
 
 describe("When user logs in with valid credentials", () => {
     before(()=> {
+        cy.eyesOpen({appName: 'hackaton-applitools', batchName: 'login-valid'});
+
         loginPage.navigate();
         loginPage.loginAs("testuser", "testpass");
     });
     
     it("Then app logs in successfully", () => {
         loginPage.checkUserIsLoggedIn("Jack Gomez");
+        cy.eyesCheckWindow('valid login - dashboard');
     });
 
     describe("And user sorts table by amount", () => {
         it("Then all entries are ordered by amount in ascending", () => {
             dashboardPage.orderTransactionsByAmount();
             dashboardPage.checkTransactiosnAreOrdered();
+            cy.eyesCheckWindow('transactions ordered');
         });
     });
 
@@ -93,6 +105,7 @@ describe("When user logs in with valid credentials", () => {
 
         it("Then chart is properly loaded", () => {
             dashboardPage.checkChartIsDisplayed();
+            cy.eyesCheckWindow('compare expenses');
         });
 
         it("and chart data is correct", () => {
@@ -102,22 +115,30 @@ describe("When user logs in with valid credentials", () => {
         describe("And user clicks show data for next year", () => {
             before(() => {
                 dashboardPage.showDataForNextYear();
+                cy.eyesCheckWindow('compare expenses with next year data');
             });
 
             it("Then next year is added to graph", () => {
                 //can't assert against number of bars and headers... as they consist of a <canvas> element
             });
         });
+
     });
+  
+    after(() => cy.eyesClose());
 });
 
 describe("When user logs in using app with ads", () => {
     before(() => {
+        cy.eyesOpen({appName: 'hackaton-applitools', batchName: 'dashboard-with-ads'});
         loginPage.navigate(true);
         loginPage.loginAs("testUser", "testPass");
     });
 
     it("Then two ads are displayed", () => {
         dashboardPage.checkAdsAreDisplayed();
+        cy.eyesCheckWindow('dashboard ads are displayed');
     });
+
+    after(() => cy.eyesClose());
 });
